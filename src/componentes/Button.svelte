6577ef853041221745c1ebca4d247c0c0f11e57b
CarlosGalaron <carlos.galaron@gmail.com>
2024-12-10 09:15:02 +0100

    add Button

---
+++ src/componentes/Button.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let children;
+</script>
+
+<button class="header-button">{children}</button>
+
+<style>
+    .header-button{
+        width: 12%;
+        height: 3em;
+        color: black;
+        background-color: white(55, 94, 201);
+        border: 2px solid rgb(7, 7, 63);
+        box-sizing: border-box;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0 4% 0 4%;
+        border-radius: 16%;
+        font-size: 16px;
+        font-weight: 1000;
+        cursor: pointer;
+    }
+
+    .header-button:hover{
+        background-color: lightcyan;
+        color: black;
+    }
+</style>
--- src/componentes/Header.svelte
+++ src/componentes/Header.svelte
@@ -1,14 +1,14 @@
 <script>
-
+    import Button from "./Button.svelte";    
 </script>
 
 <div class="header-container">
     <div class="header-icon_container"></div>
-    <button class="header-button">BIBLIOTECA</button>
-    <button class="header-button">INTERCAMBIO</button>
-    <button class="header-button">TIENDA</button>
-    <button class="header-button">PUBLICACIONES</button>
-    <button class="header-button">AUDIOLIBROS</button>
+    <Button children = "Biblioteca"></Button>
+    <Button children = "Intercambio"></Button>
+    <Button children = "Tienda"></Button>
+    <Button children = "Publicaciones"></Button>
+    <Button children = "Audiolibros"></Button>
 </div>
 
 <style>
@@ -24,27 +24,4 @@
         align-items: center;
 
     }
-
-    .header-button{
-        width: 12%;
-        height: 3em;
-        color: black;
-        background-color: white(55, 94, 201);
-        border: 2px solid rgb(7, 7, 63);
-        box-sizing: border-box;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 0 4% 0 4%;
-        border-radius: 16%;
-        font-size: 16px;
-        font-weight: 1000;
-        cursor: pointer;
-    }
-
-    .header-button:hover{
-        background-color: lightcyan;
-        color: black;
-    }
-
 </style>
